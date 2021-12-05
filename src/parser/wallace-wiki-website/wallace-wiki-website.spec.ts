1396759680a4e63f2ad7688083b5a8df63477ade
Semyon <semyonf@icloud.com>
2021-12-05 05:12:18 +0300

    Remove a useless class

--- src/parser/wallace-wiki-website/wallace-wiki-website.spec.ts
+++
@@ -1,7 +0,0 @@
-import { WallaceWikiWebsite } from './wallace-wiki-website';
-
-describe(WallaceWikiWebsite.name, () => {
-  it('should be defined', function () {
-    expect(WallaceWikiWebsite).toBeDefined();
-  });
-});
