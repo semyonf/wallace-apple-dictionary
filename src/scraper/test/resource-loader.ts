1eb536a2ef2145e0258424800a8ae987d936754c
Semyon <semyonf@icloud.com>
2022-04-29 01:47:43 +0300

    Assert resources are there before running tests

--- src/scraper/test/resource-loader.ts
+++ src/scraper/test/resource-loader.ts
@@ -1,13 +1,28 @@
-import fs from 'fs/promises';
+import fs from 'fs';
+import assert from 'assert';
+
+const resourceRoot = `${__dirname}/resources/`;
 
 export enum ResourceName {
   RawPage = 'raw-page.html',
   SingleAnnotation = 'parsed-annotation-stub.xml',
 }
 
-export function loadResource(resourceName: ResourceName): Promise<string> {
-  return fs.readFile(`${__dirname}/resources/${resourceName}`, {
+assertResourcesArePresentSync();
+
+export async function loadResource(
+  resourceName: ResourceName,
+): Promise<string> {
+  return fs.promises.readFile(`${resourceRoot}${resourceName}`, {
     encoding: 'utf-8',
     flag: 'r',
   });
+}
+
+function assertResourcesArePresentSync() {
+  const files = fs.readdirSync(resourceRoot);
+
+  Object.values(ResourceName).forEach((resource) =>
+    assert(files.includes(resource)),
+  );
 }
